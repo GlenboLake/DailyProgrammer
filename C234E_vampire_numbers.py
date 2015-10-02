552e2518e4dfb902a016833181931faf720d7720
Glen Haber <GHaber@BehaviorMatrix.com>
2015-10-02 16:34:25 -0400

    234E: Changed product to combinations_with_replacement

--- C234E_vampire_numbers.py
+++ C234E_vampire_numbers.py
@@ -1,12 +1,12 @@
 from functools import reduce
-from itertools import product
+from itertools import combinations_with_replacement
 from operator import mul
 num_digits, num_fangs = map(int, input().split())
 fang_size = num_digits // num_fangs
 
 vampires = {}
 fang_numbers = range(10**(fang_size-1), 10**fang_size)
-for fangs in product(fang_numbers, repeat=num_fangs):
+for fangs in combinations_with_replacement(fang_numbers, num_fangs):
     vampire = reduce(mul, fangs)
     if vampire in vampires:
         continue
